cf949936c11dbc4decf08ec765d2a0910cb46506
Jasmine Tsunoda <jazzyfizzle423@gmail.com>
2021-11-16 16:04:05 +0900

    Update WeatherForecastDay.js
    added space bar between max and min temps

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -26,7 +26,8 @@ export default function WeatherForecastDay(props) {
       <div className="WeatherForecast-temperatures">
         <span className="WeatherForecast-temperature-max">
           {maxTemperature()}
-        </span>
+        </span>{" "}
+        |
         <span className="WeatherForecast-temperature-min">
           {minTemperature()}
         </span>
